20e9fbceaa0f34cbd3e74a9ce8d7daed7611f018
unknown <elpidiosoares@gmail.com>
2024-01-31 18:14:33 -0300

    add

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste.</Text>
+      <Text>TeSte!!</Text>
       <StatusBar style="auto" />
     </View>
   );
