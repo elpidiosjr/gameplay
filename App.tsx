f8f97f9238010c875d40df43dee24008636df435
unknown <elpidiosoares@gmail.com>
2024-02-02 14:48:18 -0300

    novo nome

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TeSte!!</Text>
+      <Text>INHERE!!</Text>
       <StatusBar style="auto" />
     </View>
   );
