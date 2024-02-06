7a856027dd11b9ae12fd439696f2a5c0fa43a437
unknown <elpidiosoares@gmail.com>
2024-02-06 12:27:08 -0300

    stylesheet

--- App.tsx
+++ App.tsx
@@ -3,27 +3,33 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#fa7f72',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Nome do evento
         </Text>
-        <Text style={{
-        color: '#fa2f12',
-        fontSize: 16,
-      }}>
+        <Text style={styles.eventDate}>
         Segunda Feira 5 de Fevereiro de 2024
         </Text>
     </View>    
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+        color: '#fdfcfe',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+  },
+  eventDate: {
+        color: '#6b6b6b',
+        fontSize: 16,
+  }
+
+})
 
