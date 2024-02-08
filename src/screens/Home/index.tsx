c2f871b5ef2841576db95a232ccbcb624862c79e
unknown <elpidiosoares@gmail.com>
2024-02-08 12:20:54 -0300

    button and form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from './styles';
 
 export default function Home() {
+
+  function handleParticipantAdd(){
+    console.log("Voce clicou no botao de Adicionar!");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -11,6 +16,19 @@ export default function Home() {
         <Text style={styles.eventDate}>
         Segunda Feira 5 de Fevereiro de 2024
         </Text>
-    </View>    
+       <View style={styles.form}>
+        <TextInput
+         style={styles.input}
+         placeholder="Nome do participante"
+         placeholderTextColor="#6B6B6B"
+         
+         />
+    <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+     <Text style={styles.buttonText}>
+      +
+       </Text>
+      </TouchableOpacity>
+    </View>
+  </View>    
   );
 }
